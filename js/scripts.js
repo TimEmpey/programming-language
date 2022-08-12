4232032736c1b59b2ca5394d6954a7829f5e2831
Tim Empey <timothyempey@gmail.com>
2022-08-12 13:49:40 -0700

    fix typos, bugs, and spacing issues with index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,32 +10,23 @@ window.onload = function () {
     event.preventDefault()
     hideResults();
     let javaScriptOutput = document.getElementById("javascript");
-    //javaScriptOutput.setAttribute("class", "hidden");
+    javaScriptOutput.setAttribute("class", "hidden");
     let pythonOutput = document.getElementById("python");
-   // pythonOutput.setAttribute("class", "hidden");
+    pythonOutput.setAttribute("class", "hidden");
     let rubyOutput = document.getElementById("ruby");
-    //rubyOutput.setAttribute("class", "hidden");
-
-    const q1 = document.querySelector("input[name='question1']:checked".value);
-    const q2 = document.querySelector("input[name='question2']:checked".value);
-    const q3 = document.querySelector("input[name='question3']:checked".value);
-    const q4 = document.querySelector("input[name='question4']:checked".value);
-    const q5 = document.querySelector("input[name='question5']:checked".value);
+    rubyOutput.setAttribute("class", "hidden");
+    let errorOutput = document.getElementById("error")
+    errorOutput.setAttribute("class", "hidden")
 
     if (document.getElementById("dataCompilers").checked && document.getElementById("aGoodAmount").checked && document.getElementById("yes").checked && document.getElementById("good").checked && document.getElementById("tryingNewThings").checked){
       pythonOutput.removeAttribute("class");
-      console.log("pythonWorking");
-
     } else if (document.getElementById("webpages").checked && document.getElementById("noneAtAll").checked && document.getElementById("yes").checked && document.getElementById("alright").checked && document.getElementById("iLoveComputers").checked){
       javaScriptOutput.removeAttribute("class");
-      console.log ("javaWorking");
-
     } else if (document.getElementById("games").checked && document.getElementById("aLittleBit").checked && document.getElementById("yes").checked && document.getElementById("terrible").checked && document.getElementById("toMakeMoney").checked){
-      rubyOutput.removeAttribute("class")
-      console.log("rubyWorking");
-
+      rubyOutput.removeAttribute("class");
     } else {
-      window.alert("Uh Oh, looks like coding isn't for you.")
+      hideResults ();
+      errorOutput.removeAttribute("class");
       }
   };
 };
