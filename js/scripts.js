17df69392a34a5261c7b73ffcfa75c1e79900f25
Tim Empey <timothyempey@gmail.com>
2022-08-12 13:20:01 -0700

    add id to radio answers, and changed how if statement is arranged

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,11 @@ window.onload = function () {
     event.preventDefault()
     hideResults();
     let javaScriptOutput = document.getElementById("javascript");
-    javaScriptOutput.setAttribute("class", "hidden");
+    //javaScriptOutput.setAttribute("class", "hidden");
     let pythonOutput = document.getElementById("python");
-    pythonOutput.setAttribute("class", "hidden");
+   // pythonOutput.setAttribute("class", "hidden");
     let rubyOutput = document.getElementById("ruby");
-    rubyOutput.setAttribute("class", "hidden");
+    //rubyOutput.setAttribute("class", "hidden");
 
     const q1 = document.querySelector("input[name='question1']:checked".value);
     const q2 = document.querySelector("input[name='question2']:checked".value);
@@ -22,15 +22,18 @@ window.onload = function () {
     const q4 = document.querySelector("input[name='question4']:checked".value);
     const q5 = document.querySelector("input[name='question5']:checked".value);
 
-    if (q1 === "Webpages" && q2 === "None at all" && q3 === "Yes" && q4 === "Alright" && q5 === "I love computers"){
-      javaScriptOutput.removeAttribute("class");
-      console.log ("javaWorking");
-    } else if (q1 === "Data compilers" && q2 === "A good amount" && q3 === "Yes" && q4 === "Good" && q5 === "Trying new things"){
+    if (document.getElementById("dataCompilers").checked && document.getElementById("aGoodAmount").checked && document.getElementById("yes").checked && document.getElementById("good").checked && document.getElementById("tryingNewThings").checked){
       pythonOutput.removeAttribute("class");
       console.log("pythonWorking");
-    } else if (q1 === "Games" && q2 === "A little bit" && q3 === "Yes" && q4 === "Terrible" && q5 === "To make money"){
+
+    } else if (document.getElementById("webpages").checked && document.getElementById("noneAtAll").checked && document.getElementById("yes").checked && document.getElementById("alright").checked && document.getElementById("iLoveComputers").checked){
+      javaScriptOutput.removeAttribute("class");
+      console.log ("javaWorking");
+
+    } else if (document.getElementById("games").checked && document.getElementById("aLittleBit").checked && document.getElementById("yes").checked && document.getElementById("terrible").checked && document.getElementById("toMakeMoney").checked){
       rubyOutput.removeAttribute("class")
       console.log("rubyWorking");
+
     } else {
       window.alert("Uh Oh, looks like coding isn't for you.")
       }
